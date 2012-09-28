971d826dd85adb1917e991cae67eb4e5f33ec483
Kenneth Love <kenneth@gigantuan.net>
2012-09-28 16:19:01 -0700

    url; layout update; homepage template; responsive bootstrap

--- gswd/urls.py
+++ gswd/urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
+
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gswd.views.home', name='home'),
     # url(r'^gswd/', include('gswd.foo.urls')),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
