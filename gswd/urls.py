420c0b401f7f19c87ffad14969f2f73a5a7bcac5
Kenneth Love <kenneth@gigantuan.net>
2012-09-28 16:38:21 -0700

    generic views for logout and home

--- gswd/urls.py
+++ gswd/urls.py
@@ -4,18 +4,16 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 admin.autodiscover()
 
-from django.views.generic import TemplateView
+from gswd.views import LogoutView, HomeView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'gswd.views.home', name='home'),
-    # url(r'^gswd/', include('gswd.foo.urls')),
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^logout/$', LogoutView.as_view(), name="logout"),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'', include('social_auth.urls')),
+    url(r'^$', HomeView.as_view(), name="home"),
 )
 
 urlpatterns += staticfiles_urlpatterns()
